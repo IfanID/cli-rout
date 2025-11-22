bccfc837175faef5dfcd1e7fb1f0a2ed3d276fa4
IfanID <Nafantriyadi.11.99@gmail.com>
2025-11-23 00:43:23 +0700

    Initial commit

---
+++ rout.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+import json
+
+def main():
+    """
+    Fungsi utama untuk CLI rout.
+    """
+    print("Selamat datang di rout CLI!")
+
+if __name__ == "__main__":
+    main()
